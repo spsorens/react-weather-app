f3482e782eaa02fd56c65fa1783efabe370d2d77
spsorens <spsorens@gmail.com>
2021-10-07 20:51:05 -0700

    updated "noreferrer"

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,11 @@ import React from "react";
 export default function Footer() {
   return (
     <p>
-      <a href="https://github.com/spsorens/react-weather-app" target="_blank">
+      <a
+        href="https://github.com/spsorens/react-weather-app"
+        target="_blank"
+        rel="noferrer"
+      >
         Open Source Code
       </a>{" "}
       by Shannan Sorenson
