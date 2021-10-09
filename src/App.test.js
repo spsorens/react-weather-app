e474f00f715b8452d3ddbec073d30f3311d30ffe
spsorens <spsorens@gmail.com>
2021-10-09 12:10:06 -0700

    cleaned up code

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-import axios from "axios";
-
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
