59acf740f77082a29ea49f888347fc9907de3c26
spsorens <spsorens@gmail.com>
2021-10-09 12:17:57 -0700

    fixed folders

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import axios from "axios";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
