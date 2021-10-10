76540f740901de714ba0f561a03c0c6680120fb6
spsorens <spsorens@gmail.com>
2021-10-10 14:53:03 -0700

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.css";
 import Search from "./Search";
 import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
-      <Search />
-      <Footer />
+      <div className="container">
+        <h1>Weather Search Engine</h1>
+        <Search />
+        <Footer />
+      </div>
     </div>
   );
 }
